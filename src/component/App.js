d32195de2ab07f79b56cc814682ced9a6b239578
Mo Elwekil <Mo@descod.net>
2022-02-21 00:05:05 +0000

    Unsplash api is now returning data

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Form from './form'
 
 class App extends Component {
+    state = { images: [] }
 
     onFormSubmit(e) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: e },
+            headers: { Authorization: "Client-ID Yme6ZcumIXpWryQ0DPc249CE0ua2Mxh66Y-4W2gPAAc", },
+        }).then(response => {
+            this.setState({ images: response.data.results })
+        })
     }
     render() {
         return (
