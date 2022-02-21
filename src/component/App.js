5a5018b2cf9dcd295c2153285beb745cc7a7ae57
Mo Elwekil <Mo@descod.net>
2022-02-21 00:13:51 +0000

    onformsubmit function refactor

--- src/component/App.js
+++ src/component/App.js
@@ -6,18 +6,19 @@ import Form from './form'
 class App extends Component {
     state = { images: [] }
 
-    onFormSubmit(e) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    onFormSubmit = async (e) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: e },
             headers: { Authorization: "Client-ID Yme6ZcumIXpWryQ0DPc249CE0ua2Mxh66Y-4W2gPAAc", },
-        }).then(response => {
-            this.setState({ images: response.data.results })
         })
+
+        this.setState({ images: response.data.results });
     }
     render() {
+        console.log(this.state.images)
         return (
             <div>
-                <Form onSubmit={(e) => { this.onFormSubmit(e) }} />
+                <Form onSubmit={this.onFormSubmit} />
             </div>
         )
     }
